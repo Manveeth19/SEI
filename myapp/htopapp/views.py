25b6da51150aa4b3e586957c1eb8f1edc823c18a
Manveeth19 <149753522+Manveeth19@users.noreply.github.com>
2025-03-17 17:37:41 +0000

    Initial commit

---
+++ myapp/htopapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+import os
+import datetime
+import subprocess
+from django.http import HttpResponse
+
+def htop_view(request):
+    name = "Adumala Manveeth Reddy"
+    username = os.getenv("USER") or os.getenv("USERNAME")
+    ist_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S IST")
+    top_output = subprocess.getoutput("top -b -n 1 | head -10")
+
+    html = f"""
+    <h1>System Info</h1>
+    <p><strong>Name:</strong> {name}</p>
+    <p><strong>Username:</strong> {username}</p>
+    <p><strong>Server Time (IST):</strong> {ist_time}</p>
+    <pre>{top_output}</pre>
+    """
+    return HttpResponse(html)
